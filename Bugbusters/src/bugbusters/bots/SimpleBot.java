f17365df80fd901b9e238add9ba5ba0117b9265d
Florian Plähn <florian.plaehn@hbt.de>
2014-09-19 10:27:15 +0200

    Minimaler Roboter der laufen sollte

--- Bugbusters/src/bugbusters/bots/SimpleBot.java
+++ Bugbusters/src/bugbusters/bots/SimpleBot.java
@@ -28,6 +28,30 @@ public class SimpleBot extends AbstractBot {
 
 						break;
 
+					case MessageHandlerIn.INITIALIZE:
+						if ("1".equals(st.nextToken())) {
+							handlerOut
+									.sendMessage("Name S-Bot Team: BUG-BUSTERS");
+							handlerOut.sendMessage("Colour FF0000 00FF00");
+						}
+
+						break;
+					case MessageHandlerIn.GAME_STARTS:
+						handlerOut.sendMessage("Shoot 1");
+						break;
+
+					case MessageHandlerIn.EXIT_ROBOT:
+						System.exit(0);
+						break;
+
+					case MessageHandlerIn.DEAD:
+						// TODO
+						break;
+
+					case MessageHandlerIn.GAME_FINISHES:
+						// TODO
+						break;
+
 					default:
 						break;
 					}
